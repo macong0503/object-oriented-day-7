b1f944cbdfd994cfab11471707c0737400eb3402
macong0503 <1512346520@qq.com>
2021-08-03 19:35:52 +0800

    Create jisaunqi.java

---
+++ Test/Junit/jisaunqi.java
@@ -0,0 +1,10 @@
+package com.hqyj.mc.Test.Junit;
+
+public class jisaunqi {
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int sub(int a,int b){
+        return a-b;
+    }
+}
